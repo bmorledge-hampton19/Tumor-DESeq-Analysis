c2ddd30233a2fe91a659137479b4cb05fb5cfbab
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2019-11-20 13:29:52 -0800

    Removed initial filtering by log-fold change

--- Tumor Data Analysis.R
+++ Tumor Data Analysis.R
@@ -229,85 +229,6 @@ for (i in 1:length(tumorIDs)) {
 
 
 
-##### Limma-Voom #####
-
-#Create the necessary directorys for saving data.
-dir.create("SavedData/LimmaVoomObjects")
-dir.create("SavedData/LimmaVoomRawData")
-
-for (i in 1:length(tumorIDs)) {
-  
-  #Ensure that we have data to work with in the first place.  If not, skip this tumor type.
-  if (file.exists(paste0("SavedData/SizeAndGeneExpression/TCGA-",tumorIDs[i],"-CGE.rda"))) {
-    
-    # Load in Data
-    load(paste0("SavedData/SizeAndGeneExpression/TCGA-",tumorIDs[i],"-CGE.rda"))
-    
-    # Split up the data
-    sizeData = sizeAndExpressionData[,.(bcr_patient_barcode,stage_event_tnm_categories)]
-    expressionData = sizeAndExpressionData[,!"stage_event_tnm_categories"]
-    
-    # Re-Transpose expression data to traditional "counts" structure.
-    expressionData = melt(expressionData, id.vars = "bcr_patient_barcode", 
-                          variable.name = "Gene", value.name = "Counts")
-    expressionData = dcast(expressionData, Gene ~ bcr_patient_barcode, value.var = "Counts")
-    setkey(expressionData,Gene)
-    
-    # Derive normalization factors.
-    dgeList = DGEList(as.matrix(expressionData, rownames = TRUE), 
-                      group = as.vector(sizeData[,stage_event_tnm_categories]))
-    normalizedData = calcNormFactors(dgeList)
-    
-    # Filter out low expressed genes
-    normalizedAndTrimmedData = normalizedData[filterByExpr(normalizedData,),]
-    
-    # Apply the voom transformation
-    tumorSizes = sizeData[,stage_event_tnm_categories]
-    voomModel = model.matrix(~0 + tumorSizes)
-    voomResults = voom(normalizedAndTrimmedData,voomModel,plot = TRUE)
-    
-    # Use Limma to fit the linear model (and save it)
-    limmaFit = lmFit(voomResults,voomModel)
-    save(limmaFit, file = paste0("SavedData/LimmaVoomObjects/TCGA-",tumorIDs[i],"-LFit.rda"))
-    
-    # Create the contrasts we want to analyze.
-    T1vsT2Contr = makeContrasts(tumorSizesT1 - tumorSizesT2, levels = colnames(coef(limmaFit)))
-    T1vsT3Contr = makeContrasts(tumorSizesT1 - tumorSizesT3, levels = colnames(coef(limmaFit)))
-    T2vsT3Contr = makeContrasts(tumorSizesT2 - tumorSizesT3, levels = colnames(coef(limmaFit)))
-    
-    # Get the results (smoothed using empirical Bayes)
-    T1vsT2 = topTable((eBayes(contrasts.fit(limmaFit, T1vsT2Contr))), 
-                      sort.by = "none", number = Inf)
-    T1vsT3 = topTable((eBayes(contrasts.fit(limmaFit, T1vsT3Contr))), 
-                      sort.by = "none", number = Inf)
-    T2vsT3 = topTable((eBayes(contrasts.fit(limmaFit, T2vsT3Contr))), 
-                      sort.by = "none", number = Inf)
-    
-    
-    # Convert the results to neat and tidy data.tables, removing genes with low changes in expression
-    T1vsT2 = as.data.table(T1vsT2[abs(T1vsT2[,"logFC"]) > 0.58,], keep.rownames = "Gene")
-    T1vsT3 = as.data.table(T1vsT3[abs(T1vsT2[,"logFC"]) > 0.58,], keep.rownames = "Gene")
-    T2vsT3 = as.data.table(T2vsT3[abs(T1vsT2[,"logFC"]) > 0.58,], keep.rownames = "Gene")
-
-    #Create a sub-directory to save the tables in.
-    dir.create(paste0("SavedData/LimmaVoomRawData/",tumorIDs[i]))
-    
-    #Save the tables
-    save(T1vsT2, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T1vsT2.rda"))
-    save(T1vsT3, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T1vsT3.rda"))
-    save(T2vsT3, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T2vsT3.rda"))
-    
-  }
-  
-}
-
-
-
-##### Run Limma-Voom #####
-
-
-
-
 ##### Extract DESeq Results #####
 
 # Create the necessary Directorys for saving data.
@@ -324,9 +245,6 @@ createResultsTable = function(DESeqResults, genes, size1, size2) {
   #Convert the results to a data.table and add the column for gene ID's.
   DESeqResultsTable = as.data.table(results)[,Gene := genes]
   setcolorder(DESeqResultsTable,c(7,1:6))
-  
-  #Remove insignificant changes and small changes.
-  DESeqResultsTable = DESeqResultsTable[abs(log2FoldChange) > 0.58]
   
   return(DESeqResultsTable)
   
@@ -372,6 +290,80 @@ for (i in 1:length(tumorIDs)) {
   }
   
 }
+
+##### Limma-Voom #####
+
+#Create the necessary directorys for saving data.
+dir.create("SavedData/LimmaVoomObjects")
+dir.create("SavedData/LimmaVoomRawData")
+
+for (i in 1:length(tumorIDs)) {
+  
+  #Ensure that we have data to work with in the first place.  If not, skip this tumor type.
+  if (file.exists(paste0("SavedData/SizeAndGeneExpression/TCGA-",tumorIDs[i],"-CGE.rda"))) {
+    
+    # Load in Data
+    load(paste0("SavedData/SizeAndGeneExpression/TCGA-",tumorIDs[i],"-CGE.rda"))
+    
+    # Split up the data
+    sizeData = sizeAndExpressionData[,.(bcr_patient_barcode,stage_event_tnm_categories)]
+    expressionData = sizeAndExpressionData[,!"stage_event_tnm_categories"]
+    
+    # Re-Transpose expression data to traditional "counts" structure.
+    expressionData = melt(expressionData, id.vars = "bcr_patient_barcode", 
+                          variable.name = "Gene", value.name = "Counts")
+    expressionData = dcast(expressionData, Gene ~ bcr_patient_barcode, value.var = "Counts")
+    setkey(expressionData,Gene)
+    
+    # Derive normalization factors.
+    dgeList = DGEList(as.matrix(expressionData, rownames = TRUE), 
+                      group = as.vector(sizeData[,stage_event_tnm_categories]))
+    normalizedData = calcNormFactors(dgeList)
+    
+    # Filter out low expressed genes
+    normalizedAndTrimmedData = normalizedData[filterByExpr(normalizedData,),] #Filter by > 1/2 have cpm > .2
+    
+    # Apply the voom transformation
+    tumorSizes = sizeData[,stage_event_tnm_categories]
+    voomModel = model.matrix(~0 + tumorSizes)
+    voomResults = voom(normalizedAndTrimmedData,voomModel,plot = TRUE)
+    
+    # Use Limma to fit the linear model (and save it)
+    limmaFit = lmFit(voomResults,voomModel)
+    save(limmaFit, file = paste0("SavedData/LimmaVoomObjects/TCGA-",tumorIDs[i],"-LFit.rda"))
+    
+    # Create the contrasts we want to analyze.
+    T1vsT2Contr = makeContrasts(tumorSizesT1 - tumorSizesT2, levels = colnames(coef(limmaFit)))
+    T1vsT3Contr = makeContrasts(tumorSizesT1 - tumorSizesT3, levels = colnames(coef(limmaFit)))
+    T2vsT3Contr = makeContrasts(tumorSizesT2 - tumorSizesT3, levels = colnames(coef(limmaFit)))
+    
+    # Get the results (smoothed using empirical Bayes)
+    T1vsT2 = topTable((eBayes(contrasts.fit(limmaFit, T1vsT2Contr))), 
+                      sort.by = "none", number = Inf)
+    T1vsT3 = topTable((eBayes(contrasts.fit(limmaFit, T1vsT3Contr))), 
+                      sort.by = "none", number = Inf)
+    T2vsT3 = topTable((eBayes(contrasts.fit(limmaFit, T2vsT3Contr))), 
+                      sort.by = "none", number = Inf)
+    
+    
+    # Convert the results to neat and tidy data.tables, removing genes with low changes in expression
+    T1vsT2 = as.data.table(T1vsT2, keep.rownames = "Gene")
+    T1vsT3 = as.data.table(T1vsT3, keep.rownames = "Gene")
+    T2vsT3 = as.data.table(T2vsT3, keep.rownames = "Gene")
+
+    #Create a sub-directory to save the tables in.
+    dir.create(paste0("SavedData/LimmaVoomRawData/",tumorIDs[i]))
+    
+    #Save the tables
+    save(T1vsT2, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T1vsT2.rda"))
+    save(T1vsT3, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T1vsT3.rda"))
+    save(T2vsT3, file = paste0("SavedData/LimmaVoomRawData/",tumorIDs[i],"/T2vsT3.rda"))
+    
+  }
+  
+}
+
+
 
 ##### Biologically relevant analysis #####
 
